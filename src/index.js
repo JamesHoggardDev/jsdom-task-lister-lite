3e50f965d12f62af2f972274eacd2e3bb35d7615
james <jameshoggard.dev@gmail.com>
2021-07-31 12:39:16 -0400

    updated

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+    let createForm = document.querySelector('#create-task-form')
+
+    createForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        if (event.target){
+          
+          let formUl = document.querySelector("#tasks")
+          let formLi = document.createElement('li')
+            formLi.textContent = createForm['new-task-description'].value
+
+            formLi.addEventListener('click', event =>{
+                formLi.remove()
+            });
+
+            formUl.append(formLi)
+
+        } 
+    });
+
+let colorForm = document.querySelector("#priority-color")
+colorForm.addEventListener('submit', event => {
+    event.preventDefault();
+    if (event.target){
+        let list = document.querySelectorAll('li')
+          list.forEach(listItem => {
+            listItem.style.color = event.target.color.value
+          })
+       
+    }
+    event.target.reset();
+})
+
+function sortArr(){
+  let listArr = []
+  let list = document.querySelectorAll('li')
+  listArr.push(list)
+  listArr.sort()
+  
+  console.log(list)
+}
+
+sortArr()
+          
+
+
+
+
 });
